2e9f34da9a8cb410873e817ddf7052efe66033c2
Rayan <btrayan21@gmail.com>
2024-01-10 13:48:34 +0100

    Mise à jour BD

--- DATA/bd_ticketing.sql
+++ DATA/bd_ticketing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 09 jan. 2024 à 23:42
+-- Généré le : mer. 10 jan. 2024 à 13:48
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,6 +20,112 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `BD_Ticketing`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `log`
+--
+
+CREATE TABLE `log` (
+  `id_ticket` int(11) NOT NULL,
+  `login` int(32) NOT NULL,
+  `contenu` text NOT NULL,
+  `technicien` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id_ticket` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `sujet` text NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `priorite` enum('Faible','Moyen','Important','Urgent') NOT NULL,
+  `date_creation` date NOT NULL,
+  `statut` enum('Ouvert','En cours','Fermé') NOT NULL DEFAULT 'Ouvert'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `tickets`
+--
+
+INSERT INTO `tickets` (`id_ticket`, `login`, `sujet`, `description`, `priorite`, `date_creation`, `statut`) VALUES
+(24, 'gestion', 'test', 'voila le pb', 'Urgent', '2024-01-09', 'En cours');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nom` varchar(32) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mdp` varchar(32) NOT NULL,
+  `user_role` enum('utilisateur','admin_systeme','admin_web','technicien') NOT NULL DEFAULT 'utilisateur'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nom`, `login`, `email`, `mdp`, `user_role`) VALUES
+(1, 'Armand', 'armand', 'armand.clouzeau@gmail.com', '098f6bcd4621d373cade4e832627b4f6', 'utilisateur'),
+(3, 'Rayan', 'Rayan', 'rayan@gmail.com', 'e9e370c220d689a6fdff0eb557c406f3', 'utilisateur'),
+(4, 'tutu', 'tutu', 'tutu@gmail.com', 'bdb8c008fa551ba75f8481963f2201da', 'utilisateur'),
+(5, 'Admin_web', 'gestion', 'admweb@gmail.com', 'ab4f63f9ac65152575886860dde480a1', 'admin_web');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id_ticket`);
+
+--
+-- Index pour la table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id_ticket`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `log`
+--
+ALTER TABLE `log`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
