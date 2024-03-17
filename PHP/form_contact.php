c23a4c097639a7ac8cf94c9257b26c31d5e15ef3
ArmandCl <armand.clouzeau@outlook.com>
2024-03-17 18:16:08 +0100

    Rajout du style dans le fomulaire de contact

--- PHP/form_contact.php
+++ PHP/form_contact.php
@@ -1,7 +1,6 @@
 <?php
 require_once("fonction_check_connexion.php");
 
-session_start();
 
 // Générer deux chiffres aléatoires entre 1 et 10
 $nombre1 = rand(1, 10);
@@ -20,38 +19,41 @@ $_SESSION['captcha'] = $somme;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire de Contact</title>
+    <link href="../CSS/style_form_ticket.css" rel="stylesheet">
 </head>
 <body>
 
-<h2>Contactez-nous</h2>
+<div class="container">
+    <h1>Contactez-nous</h1>
 
-<form action="" method="post">
-    <label for="email">Email :</label>
-    <select id="email" name="email">
-        <option value="btrayan21@gmail.com">Directeur du Service Technique</option>
-        <option value="ismail.akboulatov@gmail.com">Administrateur de la Plateforme</option>
-        <option value="armand.clouzeau@gmail.com">Analyste des Tickets</option>
-        <option value="sarah.bader.f@gmail.com">Support Technique</option>
-        <option value="aymeric.pesenti@gmail.com">Service Clients</option>
-    </select>
+    <form action="" method="post">
+        <label for="email">Email :</label>
+        <select id="email" name="email">
+            <option value="btrayan21@gmail.com">Directeur du Service Technique</option>
+            <option value="ismail.akboulatov@gmail.com">Administrateur de la Plateforme</option>
+            <option value="armand.clouzeau@gmail.com">Analyste des Tickets</option>
+            <option value="sarah.bader.f@gmail.com">Support Technique</option>
+            <option value="aymeric.pesenti@gmail.com">Service Clients</option>
+        </select>
 
-    <br>
+        <br>
 
-    <label for="objet">Objet :</label>
-    <input type="text" id="objet" name="objet" placeholder="Raison du contact" required>
+        <label for="objet">Objet :</label>
+        <input type="text" id="objet" name="objet" placeholder="Raison du contact" required>
 
-    <br>
+        <br>
 
-    <label for="description">Description :</label>
-    <textarea id="description" name="description" rows="4" placeholder="Description du problème rencontré" required></textarea>
+        <label for="description">Description :</label>
+        <textarea id="description" name="description" rows="4" placeholder="Description du problème rencontré" required></textarea>
 
-    <br>
+        <br>
 
-    <label for="captcha"> Vérification (calculez <?php echo $nombre1; ?> + <?php echo $nombre2; ?>) :</label>
-    <input type="text" id="captcha" placeholder="Résultat" name="captcha">
+        <label for="captcha"> Vérification (calculez <?php echo $nombre1; ?> + <?php echo $nombre2; ?>) :</label>
+        <input type="text" id="captcha" placeholder="Résultat" name="captcha">
 
-    <input type="submit" value="Envoyer">
-</form>
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
 
 </body>
 </html>
